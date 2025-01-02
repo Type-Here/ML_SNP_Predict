2afcabe59dd56362741314ac5e9c24bde99af5e7
Type-Here <94553428+Type-Here@users.noreply.github.com>
2025-01-02 20:08:55 +0100

    added file to check tensorflow and keras versions

---
+++ tensor_check.py
@@ -0,0 +1,11 @@
+import tensorflow as tf
+import keras
+#from tensorflow.keras.models import Sequential
+#from tensorflow.keras.layers import Dense
+
+print(f"Tensorflow: {tf.__version__}")
+print(f"Tensorflow - Keras: {tf.keras.__version__}")
+print(f"Keras p- Standalone: {keras.__version__}")
+
+
+print("TensorFlow importato correttamente!")
