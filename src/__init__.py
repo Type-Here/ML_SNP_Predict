97209204c6747182e50ff9433d28788eaec33668
Type-Here <94553428+Type-Here@users.noreply.github.com>
2025-01-02 23:47:51 +0100

    config: added global variables for NM, Accession and fasta_path

--- src/__init__.py
+++ src/__init__.py
@@ -2,5 +2,5 @@
 
 # Explicit Imports
 from .data_load import load_data
-from .config import DATA_PATH, P53_MODEL_NAME, P53_MODEL_DIR
+from .config import DATA_PATH, P53_MODEL_NAME, P53_MODEL_DIR, P53_NM, P53_ACCESSION, HRAS_NM, HRAS_ACCESSION, FASTA_PATH
 from .utility import check_column_types, print_columns_with_nan, print_nan_rows, print_unique_values
--- src/config.py
+++ src/config.py
@@ -5,6 +5,18 @@ import os
 
 # Data directory path
 DATA_PATH = os.path.join(os.path.dirname(__file__), '../data/')
+# Fasta files directory path
+FASTA_PATH = os.path.join(os.path.dirname(__file__), 'sequences/')
 
+# p53 variables
 P53_MODEL_NAME = 'p53_model'
 P53_MODEL_DIR = os.path.join(os.path.dirname(__file__), '../models/')
+
+P53_NM = 'NM_000546.6'
+P53_ACCESSION = 'P04637'
+
+# HRAS variables
+HRAS_NM = 'NM_005343.2'
+HRAS_ACCESSION = 'P01112'
+
+
