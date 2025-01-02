9191ef99f81f409f4e8a408b16bb0624c108cbcd
Type-Here <94553428+Type-Here@users.noreply.github.com>
2025-01-02 16:18:00 +0100

    p53_scaling: added data normalization for cDNA_Position

---
+++ src/p53/p53_scaling.py
@@ -0,0 +1,27 @@
+"""
+    This module contains the functions to scale and normalize data in the p53 dataset.
+"""
+
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+
+def dna_position_norm(data:pd.DataFrame) -> pd.DataFrame:
+    """
+        Normalize the cDNA_Position data.\n
+        Normalize the DNA position data to the range [0, 1].
+    """
+    
+
+    # List of numerical columns to normalize
+    numerical_columns = ['cDNA_Position']
+
+    # Apply MinMaxScaler
+    scaler = MinMaxScaler()
+    data[numerical_columns] = scaler.fit_transform(data[numerical_columns])
+
+    # Verify the normalized dataset
+    print("\nDataset after normalizing numerical columns:")
+    print(data[numerical_columns].head())
+
+    return data
+
