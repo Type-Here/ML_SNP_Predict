62b195efe740858fae50457cc98d09db157b424f
Type-Here <94553428+Type-Here@users.noreply.github.com>
2025-01-02 17:32:09 +0100

    added variables for versioning, added keras dep install

--- tensor.py
+++ tensor.py
@@ -1,21 +1,26 @@
 import os
 import subprocess
 
+TF_VERSION = "2.17.1"
+KERAS_VERSION = "3.5.0"
+ROCM_VERSION = "6.3"
+
 def install_tensorflow():
     try:
         # Verifica se esiste una GPU NVIDIA
         nvidia_check = subprocess.run(['nvidia-smi'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         if nvidia_check.returncode == 0:
             print("NVIDIA GPU detected. Installing TensorFlow with CUDA support...")
-            os.system("pip install tensorflow==2.17.1")
+            os.system(f"pip install tensorflow-gpu=={TF_VERSION}")
+            os.system(f"pip install keras=={KERAS_VERSION}"})
             return
 
         # Verifica se esiste una GPU AMD
         rocm_check = subprocess.run(['rocminfo'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         if rocm_check.returncode == 0:
             print("AMD GPU detected. Installing TensorFlow with ROCm support...")
-            rocm_version = "6.3"  # Cambia con la versione ROCm installata
-            os.system(f"pip install tensorflow-rocm==2.17.1 -f https://repo.radeon.com/rocm/manylinux/rocm-rel-{rocm_version}/")
+            os.system(f"pip install tensorflow-rocm=={TF_VERSION} -f https://repo.radeon.com/rocm/manylinux/rocm-rel-{ROCM_VERSION}/")
+            os.system(f"pip install keras=={KERAS_VERSION}")
             return
 
     except FileNotFoundError:
@@ -23,7 +28,8 @@ def install_tensorflow():
 
     # Nessuna GPU rilevata, installa TensorFlow per CPU
     print("No GPU detected. Installing TensorFlow for CPU...")
-    os.system("pip install tensorflow==2.17.1 --no-deps")
+    os.system(f"pip install tensorflow=={TF_VERSION}")
+    os.system(f"pip install keras=={KERAS_VERSION}")
 
 if __name__ == "__main__":
     install_tensorflow()
