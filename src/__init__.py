37acc2bd07c13d4795b77a9c5418dd3b5ce55c80
Type-Here <94553428+Type-Here@users.noreply.github.com>
2025-01-02 18:00:13 +0100

    added global variables for p53 model dir and name

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,6 @@
 # src/__init__.py
 
 # Explicit Imports
-from .data_prep import load_data
-from .config import DATA_PATH
-from .utility import check_column_types
+from .data_load import load_data
+from .config import DATA_PATH, P53_MODEL_NAME, P53_MODEL_DIR
+from .utility import check_column_types, print_columns_with_nan, print_nan_rows, print_unique_values
--- src/config.py
+++ src/config.py
@@ -5,3 +5,6 @@ import os
 
 # Data directory path
 DATA_PATH = os.path.join(os.path.dirname(__file__), '../data/')
+
+P53_MODEL_NAME = 'p53_model'
+P53_MODEL_DIR = os.path.join(os.path.dirname(__file__), '../models/')
