559110b0ed64369ce21722e4f0011ca0a6e6b7f2
Type-Here <94553428+Type-Here@users.noreply.github.com>
2025-01-04 16:43:55 +0100

    UI/info: added style.css macro path, main colors code variables

--- UI/info.py
+++ UI/info.py
@@ -2,3 +2,13 @@ VERSION=0.2
 AUTHOR='Type-Here'
 
 LOGO_PATH = 'UI/assets/logo.png'
+
+
+# UI STYLE
+
+# Main window style
+MAIN_BACKGROUND = "#2a2e32"
+LOG_BACKGROUND = "1b1e20"
+FRAME_BORDER = "#4d4d4d"
+
+STYLE_PATH= "UI/style/style.css"
