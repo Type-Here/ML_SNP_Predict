9d9e25d4784b8a9c14542f38c87c47eb4e2cef93
Type-Here <94553428+Type-Here@users.noreply.github.com>
2025-01-06 03:41:03 +0100

    p53 model v2: passed from Sequential to Functional model, corrected input transformation, added pfam support

--- src/p53/p53_6_model_v2.py
+++ src/p53/p53_6_model_v2.py
@@ -9,7 +9,7 @@ import numpy as np
 import pandas as pd
 #from tensorflow.python.keras.models import Sequential
 #from tensorflow.python.keras.layers import Dense, Dropout
-from keras.api.models import Sequential
+from keras.api.models import Sequential, Model
 from keras.api.layers import Dense, Dropout, Input, Embedding, Flatten, Concatenate
 from keras.api.callbacks import EarlyStopping
 from src import MODELS_DIR, P53_MODEL_NAME
@@ -24,121 +24,110 @@ AA21_columns = ['WT AA_1','Mutant AA_1']
 
 # -------------------------------------------- TRAIN MODEL -------------------------------------------- #
 
-def p53_train_model(X_train, y_train, X_test, y_test) -> tuple[tf.keras.Model, tf.keras.callbacks.History]:
+def p53_train_model(X_train, y_train, X_test, y_test, pfam=False) -> tuple[tf.keras.Model, tf.keras.callbacks.History]:
     """
-        Create and train the p53 MLP model.
-        Model architecture:
-            - Embedding layer for each of the columns
-            - Dropout 0.3
-            - 128 units, ReLU activation
-            - Dropout 0.3
-            - 64 units, ReLU activation
-            - Dropout 0.3
-            - Output layer, 3 units, softmax activation
+    Create and train the p53 MLP model.
+    Model architecture:
+        - Embedding layer for each of the columns
+        - Dropout 0.3
+        - 128 units, ReLU activation
+        - Dropout 0.3
+        - 64 units, ReLU activation
+        - Dropout 0.3
+        - Output layer, 3 units, softmax activation
 
-        Parameters:
-            X_train: The training features.
-            y_train: The training labels.
-            X_test: The test features.
-            y_test: The test labels.
-        Returns:
-            The trained model and the training history.
+    Parameters:
+        X_train: The training features.
+        y_train: The training labels.
+        X_test: The test features.
+        y_test: The test labels.
+        pfam: If True, the model will be trained with Pfam data. Default is False.
+    Returns:
+        The trained model and the training history.
     """
-    input_layer = __add_input_layer()
-    model = Sequential([
-        input_layer,
-        #Dense(64, activation='relu'),
-        #Dropout(0.3),
-        Dense(128, activation='relu'),
-        Dropout(0.3),  # Prevent overfitting
-        Dense(64, activation='relu'),
-        Dropout(0.3),
-        Dense(3, activation='softmax')  # Adjust the output units for the number of classes
-    ])
+    # Create input layers
+    input_layers, concat = __add_input_layer(pfam)
+    
+    # Define dense layers
+    x = Dense(128, activation='relu')(concat)
+    x = Dropout(0.3)(x)
+    x = Dense(64, activation='relu')(x)
+    x = Dropout(0.3)(x)
+    output = Dense(3, activation='softmax')(x)
+
+    # Build the model
+    model = Model(inputs=input_layers, outputs=output)
 
     # Compile the model
     model.compile(
         optimizer='adam',
-        loss='categorical_crossentropy',  # For multiclass classification
+        loss='categorical_crossentropy',
         metrics=['accuracy']
     )
 
-     # Prepara il dizionario dei dati
-    X_train_dict = __input_dict_prepare(X_train)
-    X_test_dict = __input_dict_prepare(X_test)
-
+    # Prepare the data dictionary
+    X_train_dict = input_dict_prepare(X_train, pfam)
+    X_test_dict = input_dict_prepare(X_test, pfam)
 
     # Train the model
     history = model.fit(
         x=X_train_dict,
-        y=tf.keras.utils.to_categorical(y_train),  # Convert labels to one-hot
+        y=tf.keras.utils.to_categorical(y_train),
         validation_data=(X_test_dict, tf.keras.utils.to_categorical(y_test)),
-        epochs=50,  # Adjusted based on performance
+        epochs=50,
         batch_size=32,
         verbose=1,
         callbacks=[__early_stopping()]
     )
 
-    # Evaluate the model
-    #print("\nTest Evaluation:")
-    #print(model.evaluate(X_test, tf.keras.utils.to_categorical(y_test)))
-
     return model, history
+
 
 
 # -------------------------------------------- AIDE FUNCTIONS -------------------------------------------- #
 
 
-def __add_input_layer() -> tf.keras.layers.Layer:
+def __add_input_layer(pfam:bool) -> tf.keras.layers.Layer:
     """
         Add Input Layer for Embeddings to the model.
         Parameters:
             model: The model to add the input layer to.
             dataset: The dataset to get the input shape from.
+            pfam: If True, the model will be trained with Pfam data. Default is False.
         Returns:
             The model with the input layer added.
     """
-
     input_layers = []
 
-    # Input layers 
-    for column in N5_columns:
-        input_layers.append(Input(shape=(1,), name=column + '_Encoded'))
-    for column in N4_columns:
-        input_layers.append(Input(shape=(1,), name=column + '_Encoded'))
-    for column in AA21_columns:
+    # Input layers for encoded columns
+    for column in N5_columns + N4_columns + AA21_columns:
         input_layers.append(Input(shape=(1,), name=column + '_Encoded'))
 
     # Embedding layers
     embedding_layers = []
-    
     for i, column in enumerate(N5_columns):
         embedding = Embedding(input_dim=5, output_dim=2)(input_layers[i])
-        flat = Flatten()(embedding)
-        embedding_layers.append(flat)
-    
+        embedding_layers.append(Flatten()(embedding))
+
     for i, column in enumerate(N4_columns):
         embedding = Embedding(input_dim=4, output_dim=2)(input_layers[len(N5_columns) + i])
-        
-        if column == 'cDNA_Mut':  # Add Weight to cDNA_Mut
-            weighted_flat = tf.keras.layers.Lambda(lambda x: x * 2.0)(flat)  # NOTE: This weight can be adjusted
-            embedding_layers.append(weighted_flat)
-        else:
-            embedding_layers.append(Flatten()(embedding))
-    
+        embedding_layers.append(Flatten()(embedding))
+
     for i, column in enumerate(AA21_columns):
         embedding = Embedding(input_dim=21, output_dim=5)(input_layers[len(N5_columns) + len(N4_columns) + i])
         embedding_layers.append(Flatten()(embedding))
 
-
-    
     # Numerical inputs
-    numerical_input = Input(shape=(1,), name='cDNA_Position')  # 2 Numerical inputs: cDNA_Position
+    if pfam:
+        numerical_input = Input(shape=(2,), name='Position and Conservation')
+    else:
+        numerical_input = Input(shape=(1,), name='cDNA_Position')
+    input_layers.append(numerical_input)
     embedding_layers.append(numerical_input)
 
     # Concatenate embeddings and numerical input
     concat = Concatenate()(embedding_layers)
-    return concat
+    return input_layers, concat
 
 
 def __early_stopping():
@@ -154,18 +143,31 @@ def __early_stopping():
 )
 
 
-def __input_dict_prepare(X: pd.DataFrame) -> dict:
+def input_dict_prepare(X: pd.DataFrame, pfam=False) -> dict:
     """
-        Prepare the dictionary of data for the model.
-        Parameters:
-            X: The features to predict.
-        Returns:
-            dict: The dictionary of data for the model.
+    Prepare the dictionary of data for the model.
+    
+    Parameters:
+        X: pd.DataFrame
+            The features to predict.
+        pfam: bool
+            If True, the model will be trained with Pfam data. Default is False.
+    
+    Returns:
+        dict: The dictionary of data for the model.
     """
+    # Initialize the dictionary with encoded columns
     X_dict = {column + '_Encoded': X[column + '_Encoded'] \
-                    for column in N5_columns + N4_columns + AA21_columns}
-    X_dict['cDNA_Position'] = X['cDNA_Position']
+              for column in N5_columns + N4_columns + AA21_columns}
+    
+    # Add numerical inputs based on pfam flag
+    if pfam:
+        X_dict['Position and Conservation'] = X[['cDNA_Position', 'Conservation']].to_numpy()
+    else:
+        X_dict['cDNA_Position'] = X['cDNA_Position'].to_numpy()
+    
     return X_dict
+
 
 
 # -------------------------------------------- SAVE MODEL -------------------------------------------- #
@@ -191,11 +193,16 @@ def save_model(model: tf.keras.Model, name: str = P53_MODEL_NAME):
 # -------------------------------------------- RETRAIN MODEL FOR SAVE -------------------------------------------- #
 
 
-def retrain_model_to_save(model, X_train, y_train) -> tuple[tf.keras.Model, tf.keras.callbacks.History]:
+def retrain_model_to_save(model, X_train, y_train, pfam = False) -> tuple[tf.keras.Model, tf.keras.callbacks.History]:
     """
         Train a model with all the given data in order to save it.
         It does not split the data into training and test sets.
         It doesn't save the model.
+        Parameters:
+            model: The model to retrain.
+            X_train: The features to train the model.
+            y_train: The labels to train the model.
+            pfam: If True, the model will be trained with Pfam data. Default is False.
         Returns:
             The trained model and the training history.
     """
@@ -203,7 +210,7 @@ def retrain_model_to_save(model, X_train, y_train) -> tuple[tf.keras.Model, tf.k
     model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
     history = model.fit(
-        x = __input_dict_prepare(X_train), 
+        x = input_dict_prepare(X_train,pfam), 
         y = tf.keras.utils.to_categorical(y_train), 
         epochs=50, 
         batch_size=32, 
@@ -232,6 +239,6 @@ def model_predict(model: tf.keras.Model, X: pd.DataFrame) -> tuple[tf.Tensor, np
     #X_tensor = tf.convert_to_tensor(X.values, dtype=tf.float32)
 
     # Predict Probabilities
-    probabilities = model.predict(__input_dict_prepare(X))
+    probabilities = model.predict(input_dict_prepare(X))
 
     return probabilities, tf.argmax(probabilities, axis=1).numpy()
