1125359806dde1ba31cffaa78655417f6ac9beec
Type-Here <94553428+Type-Here@users.noreply.github.com>
2025-01-06 21:53:09 +0100

    p53 data split: added comment to create_train_test_sets function

--- src/p53/p53_5_data_split_train.py
+++ src/p53/p53_5_data_split_train.py
@@ -3,12 +3,20 @@
     5. This module should be the fifth module in the pipeline.
 """
 
-
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
 
 def create_train_test_sets(X_data, y_labels) -> tuple:
+    """
+        Splits the input data into training and test sets.
+        Parameters:
+            X_data (pd.DataFrame): The input features.
+            y_labels (pd.Series): The target labels.
+        Returns:
+            tuple: A tuple containing the training features (X_train), test features (X_test),
+                training labels (y_train), and test labels (y_test).
+    """
     # Split into training and test sets
     X_train, X_test, y_train, y_test = train_test_split(X_data, y_labels, test_size=0.2, random_state=42)
 
