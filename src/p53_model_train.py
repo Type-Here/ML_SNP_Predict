223dfd00c3e0568aa5c09b74f5711f85f97abc73
Type-Here <94553428+Type-Here@users.noreply.github.com>
2025-01-06 03:44:31 +0100

    p53_model_train: added pfam support, corrected input management for v2 model

--- src/p53_model_train.py
+++ src/p53_model_train.py
@@ -5,30 +5,69 @@ from PyQt5.QtCore import QThread, pyqtSignal
 from UI.stream.stream_redirect import StreamRedirector
 
 from src.dataset_file_management import load_data, save_processed_data
-from src.config import P53_MODEL_NAME
+from src.pfam.pfam_json import get_pfam_data, create_domain_dict, assign_conservation
+from src.config import P53_MODEL_NAME, P53_PFAM_MODEL_NAME, HRAS_MODEL_NAME
 
 import src.p53.p53_1_data_prep as p53_1_data_prep
 import src.p53.p53_2_encoding as p53_2_encoding
 import src.p53.p53_3_scaling as p53_3_scaling
 import src.p53.p53_4_data_balancing as p53_4_data_balancing
 import src.p53.p53_5_data_split_train as p53_split
-import src.p53.p53_6_model as p53_6_model
+import src.p53.p53_6_model_v2 as p53_6_model
 import src.model_evaluation as ev
 
 class TrainingThread(QThread):
     log_signal = pyqtSignal(str)  # Signal to send log messages to the main thread (GUI)
 
+
+    def __init__(self, model_name, set_pfam = False, parent=None):
+        super().__init__(parent)
+        self.use_pfam = set_pfam
+        self.model_name = model_name
+
+
     def run(self):
         """
             Run the training process in a separate thread.
         """
-        
+        if self.model_name == HRAS_MODEL_NAME:
+            #self.__train_hras_model()
+            self.log_signal.emit("Error: HRAS model not implemented yet.")
+            return
+
+        elif self.model_name == P53_MODEL_NAME:
+            self.__train_p53_model()
+
+        elif self.model_name == P53_PFAM_MODEL_NAME:
+            if not self.use_pfam:
+                self.log_signal.emit("Error: Pfam data is set to False.")
+                self.log_signal.emit("To train the Pfam model, set Pfam data to True.")
+                self.log_signal.emit("Error: Training failed.")
+                return
+            self.__train_p53_model()
+                
+        else:
+            self.log_signal.emit(f"Error: Model name not recognized: {self.model_name}")
+            self.log_signal.emit("Error: Training failed.")
+
+
+    def __train_p53_model(self):
+        """
+            Train the p53 model.
+        """
+
         # Redirect stdout and stderr to capture Keras output
         stream_redirector = StreamRedirector()
         stream_redirector.log_signal.connect(self.log_signal)  # Connect to main thread
         sys.stdout = stream_redirector
         sys.stderr = stream_redirector
 
+        if self.use_pfam:
+            used_model_name = P53_PFAM_MODEL_NAME
+        else:
+            used_model_name = P53_MODEL_NAME
+
+        self.log_signal.emit(f"<b>Note:</b> Pfam use set to: {self.use_pfam}\n")
 
         try:            
             self.log_signal.emit("Starting the training process...\n")
@@ -44,26 +83,34 @@ class TrainingThread(QThread):
             self.log_signal.emit("Preprocessing the data...")
             
             self.log_signal.emit("Cleaning the data...")
-            data = p53_1_data_prep.p53_cleaning(data)
+            data = p53_1_data_prep.p53_cleaning(data, pfam=self.use_pfam)
 
-            data_save_path = save_processed_data(data, P53_MODEL_NAME) # Save the processed data Not Encoded and Scaled
+            # Add `Conservation` column to the dataset and drop `Domain` column
+            if self.use_pfam:
+                data = p53_1_data_prep.add_pfam_conservation(data)
+
+            # Save the processed dataset (not encoded nor scaled) in CSV format
+            data_save_path = save_processed_data(data, used_model_name) # Save the processed data Not Encoded nor Scaled
             if data_save_path:
                 self.log_signal.emit(f"Processed non encoded dataset saved to:  \n{data_save_path}\n")
             else:
                 self.log_signal.emit("Error saving processed non encoded dataset.")
             
+            # Encode and scale the data
             self.log_signal.emit("Encoding the data...")
-            data = p53_2_encoding.p53_encoding(data)
+            data = p53_2_encoding.p53_encoding(data, pfam=self.use_pfam)
             
             self.log_signal.emit("Scaling the data...")
             data = p53_3_scaling.p53_scaling(data)
 
-            data_save_path = save_processed_data(data, P53_MODEL_NAME, is_encoded=True, ) # Save the processed data Encoded and Scaled but not Balanced
+            # Save the processed dataset (encoded and scaled) in CSV format
+            data_save_path = save_processed_data(data, used_model_name, is_encoded=True, ) # Save the processed data Encoded and Scaled but not Balanced
             if data_save_path:
                 self.log_signal.emit(f"Processed Encoded dataset saved to:  \n{data_save_path}\n")
             else:
                 self.log_signal.emit("Error saving processed Encoded dataset.")
 
+            # Balance and Split the data
             self.log_signal.emit("Balancing the data...")
             X_resampled, y_resampled = p53_4_data_balancing.balance_split_data(data)
 
@@ -73,13 +120,15 @@ class TrainingThread(QThread):
             
             # Train the model
             self.log_signal.emit("Training the model...")
-            model, history = p53_6_model.p53_train_model(X_train, y_train, X_test, y_test)
+            model, history = p53_6_model.p53_train_model(X_train, y_train, X_test, y_test, pfam=self.use_pfam)
 
             # Evaluate the model
             self.log_signal.emit("Evaluating the model...")
 
             # self.log_signal.emit("\nModel history - Accuracy plot:")
             # ev.plot_accuracy(history) # Unable to plot in the current environment
+
+            X_test = p53_6_model.input_dict_prepare(X_test, pfam=self.use_pfam)
 
             self.log_signal.emit("\nModel evaluation:")
             ev.simple_evaluate_model(model, X_test, y_test)
@@ -90,32 +139,34 @@ class TrainingThread(QThread):
                 X_test=X_test, 
                 y_test=y_test,
                 history=history,
-                model_name=P53_MODEL_NAME
+                model_name=used_model_name
             )
 
-            plot_acc = ev.plot_accuracy(history, model_name=P53_MODEL_NAME)            
-            ev.save_plot_incremental(plot_acc, model_name=P53_MODEL_NAME)
+            plot_acc = ev.plot_accuracy(history, model_name=used_model_name)            
+            ev.save_plot_incremental(plot_acc, model_name=used_model_name)
 
-            plot_loss = ev.plot_loss(history, model_name=P53_MODEL_NAME)
-            ev.save_plot_incremental(plot_loss, model_name=P53_MODEL_NAME)
+            plot_loss = ev.plot_loss(history, model_name=used_model_name)
+            ev.save_plot_incremental(plot_loss, model_name=used_model_name)
 
-            plot_auc = ev.plot_curve_auc(model, X_test, y_test, model_name=P53_MODEL_NAME)
-            ev.save_plot_incremental(plot_auc, model_name=P53_MODEL_NAME)
+            plot_auc = ev.plot_curve_auc(model, X_test, y_test, model_name=used_model_name)
+            ev.save_plot_incremental(plot_auc, model_name=used_model_name)
+
+
 
             self.log_signal.emit("\nCross-validation evaluation:")
-            ev.n_times_k_fold_eval(model, X_resampled, y_resampled, n_splits=10, n_repeats=2)
+            ev.n_times_k_fold_eval(model, X_resampled, y_resampled, n_splits=10, n_repeats=2, pfam=self.use_pfam)
 
             self.log_signal.emit("Training complete!\n ------------------- \n")
 
             # Train the model to save it
             self.log_signal.emit("Training the model with all the data to save it...")
 
-            model, history = p53_6_model.retrain_model_to_save(model, X_resampled, y_resampled)
+            model, history = p53_6_model.retrain_model_to_save(model, X_resampled, y_resampled, pfam=self.use_pfam)
 
             # Save the model
             self.log_signal.emit("\nSaving the model...")
 
-            p53_6_model.save_model(model)
+            p53_6_model.save_model(model, name=used_model_name)
 
             self.log_signal.emit("Model saved.")
 
@@ -136,3 +187,8 @@ class TrainingThread(QThread):
             # Restore stdout and stderr
             sys.stdout = sys.__stdout__
             sys.stderr = sys.__stderr__
+    
+
+    def set_use_pfam(self, set_pfam: bool):
+        self.use_pfam = set_pfam
+        self.log_signal.emit(f"Use Pfam: {self.use_pfam}")
